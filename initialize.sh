ddb60a2bda1af10118507086e1137f3e2d65cde3
Valerio Martini <valerio.martini@gmail.com>
2020-09-21 23:17:47 +0200

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -10,27 +10,38 @@ echo ---------------------------------------------------------------------------
 cd
 
 sudo apt-get update
+
 sudo apt install net-tools
+
 sudo apt install ssh
 
 sudo apt-get install software-properties-common
+
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 
 sudo apt install -y virtualenv
+
 virtualenv -p python3.6 ~/opt/dellos10env
+
 source ~/opt/dellos10env/bin/activate
+
 pip3 install ansible
+
 source ~/opt/dellos10env/bin/activate
 
 sudo apt autoremove
 
 git clone https://github.com/val3r1o/dellos10conf-evpn.git
+
 git clone https://github.com/val3r1o/dellos10conf-vlanvni.git
 
 ansible --version
 
+echo --------------------------------------------------------------------------------------------------------------
+
 ls
 
+echo --------------------------------------------------------------------------------------------------------------
 echo -------------------------------------- Thats all folks "!!" -----------------------------------------------------
 echo ------------------------------------valerio.martini@gmail.com ---------------------------------------------------
 echo -----------------------------------------------------------------------------------------------------------------
