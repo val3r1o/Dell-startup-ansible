ab4531e3184fb7d36934f5f2e2d35c0ae81dddf4
Valerio Martini <valerio.martini@gmail.com>
2020-09-21 23:35:58 +0200

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,42 +1,26 @@
 #!/bin/sh
 # This is part of automatic Lab setup related to https://github.com/val3r1o/startupdellos10ansible_env.git
 # FRESH UBUNTU 18.04 ##
-
 echo -----------------------------------------------------------------------------------------------------------------
 echo ---------------------------- This is an automatic installation of Ansible/Python component ----------------------
 echo ---------------------------------------------------- NETWORKING -------------------------------------------------
 echo -----------------------------------------------------------------------------------------------------------------
-
 cd
-
 sudo apt-get update
-
 sudo apt install net-tools
-
 sudo apt install ssh
-
 sudo apt-get install software-properties-common
-
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-
 sudo apt install -y virtualenv
-
 virtualenv -p python3.6 ~/opt/dellos10env
-
-source ~/opt/dellos10env/bin/activate
-
 pip3 install ansible
-
 sudo apt autoremove
-
 git clone https://github.com/val3r1o/dellos10conf-evpn.git
-
 git clone https://github.com/val3r1o/dellos10conf-vlanvni.git
-
 source ~/opt/dellos10env/bin/activate
 
 ansible --version
-
+echo 
 echo -----------------------------------------------------------------------------------------------------------------
 echo -------------------------------------- Thats all folks "!!" -----------------------------------------------------
 echo ----------------------------------- valerio.martini@gmail.com ---------------------------------------------------
