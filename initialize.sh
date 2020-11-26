298e4930c9443d44b691670998b5bdaa4f213742
Valerio Martini <valerio.martini@gmail.com>
2020-11-26 14:53:45 +0100

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -6,28 +6,37 @@ echo ---------------------------- This is an automatic installation of Ansible/P
 echo ---------------------------------------------------- NETWORKING -------------------------------------------------
 echo -----------------------------------------------------------------------------------------------------------------
 cd
+echo ----------------------------------------------------------- sudo apt update
 sudo apt update
-echo sudo apt update
+echo ----------------------------------------------------------- sudo apt install net-tools
 sudo apt install net-tools
+echo ----------------------------------------------------------- sudo apt purge openssh-client
 sudo apt purge openssh-client
+echo ----------------------------------------------------------- sudo apt purge openssh-server
 sudo apt purge openssh-server
+echo ----------------------------------------------------------- sudo apt install openssh-server
 sudo apt install openssh-server
+echo ----------------------------------------------------------- sudo apt-get install software-properties-common
 sudo apt-get install software-properties-common
+echo ----------------------------------------------------------- sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-add-repository --yes --update ppa:ansible/ansible
+echo ----------------------------------------------------------- sudo apt install -y virtualenv
 sudo apt install -y virtualenv
+echo ----------------------------------------------------------- virtualenv -p python3.6 ~/opt/dellos10env
 virtualenv -p python3.6 ~/opt/dellos10env
 cd
+echo ----------------------------------------------------------- pip3 install ansible
 pip3 install ansible
+echo ----------------------------------------------------------- pip3 install paramiko
 pip3 install paramiko
+echo ----------------------------------------------------------- sudo apt autoremove
 sudo apt autoremove
-
-# add MORE REPOSITORY 
-#git clone https://github.com/Dell-Networking/ansible-dellos-examples.git
-
+echo ----------------------------------------------------------- add MORE REPOSITORY 
+git clone https://github.com/Dell-Networking/ansible-dellos-examples.git
 cd
 cd
+echo ----------------------------------------------------------- source /home/valerio/opt/dellos10env/bin/activate
 source /home/valerio/opt/dellos10env/bin/activate
-
 echo 
 ansible --version
 echo
