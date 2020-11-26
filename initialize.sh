0a8111a6760b2d6ddb01e110a3dd1586ad635a0f
Valerio Martini <valerio.martini@gmail.com>
2020-11-26 16:18:14 +0100

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -10,10 +10,10 @@ echo ----------------------------------------------------------- sudo apt update
 sudo apt update
 echo ----------------------------------------------------------- sudo apt install net-tools
 sudo apt install net-tools
-echo ----------------------------------------------------------- sudo apt purge openssh-client
-sudo apt purge openssh-client
-echo ----------------------------------------------------------- sudo apt purge openssh-server
-sudo apt purge openssh-server
+#echo ----------------------------------------------------------- sudo apt purge openssh-client
+#sudo apt purge openssh-client
+#echo ----------------------------------------------------------- sudo apt purge openssh-server
+#sudo apt purge openssh-server
 echo ----------------------------------------------------------- sudo apt install openssh-server
 sudo apt install openssh-server
 echo ----------------------------------------------------------- sudo apt-get install software-properties-common
@@ -24,6 +24,8 @@ echo ----------------------------------------------------------- sudo apt instal
 sudo apt install -y virtualenv
 echo ----------------------------------------------------------- virtualenv -p python3.6 ~/opt/dellos10env
 virtualenv -p python3.6 ~/opt/dellos10env
+echo ----------------------------------------------------------- source ~/opt/dellos10env/bin/activate
+source ~/opt/dellos10env/bin/activate
 echo ----------------------------------------------------------- sudo apt install python3-pip
 sudo apt install python3-pip
 echo ----------------------------------------------------------- pip3 install ansible
@@ -34,10 +36,14 @@ echo ----------------------------------------------------------- sudo apt autore
 sudo apt autoremove
 echo ----------------------------------------------------------- add MORE REPOSITORY 
 git clone https://github.com/Dell-Networking/ansible-dellos-examples.git
+echo ----------------------------------------------------------- source ~/opt/dellos10env/bin/activate
+source ~/opt/dellos10env/bin/activate
+echo ----------------------------------------------------------- ansible --version
+ansible --version
 echo
 echo -----------------------------------------------------------------------------------------------------------------
 echo -----------------------------------------------------------------------------------------------------------------
 echo -------------------------------------- Thats all folks "!!" -----------------------------------------------------
-echo ----------------------------------- valerio.martini@gmail.com ---------------------------------------------------
+echo ----------------------------------- valerio.martini<at>gmail.com ---------------------------------------------------
 echo -----------------------------------------------------------------------------------------------------------------
 echo -----------------------------------------------------------------------------------------------------------------
